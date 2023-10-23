6858dbe56dd02853706e2a67fe7adfa4cc68e255
romina.fredes <romina.fredes@estudiantes.utec.edu.uy>
2023-10-23 08:09:57 -0300

    Prueba

---
+++ Probando GITHUB/Probando GITHUB/main.c
@@ -0,0 +1,18 @@
+/*
+ * Probando GITHUB.c
+ *
+ * Created: 23/10/2023 8:09:05
+ * Author : romin
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
