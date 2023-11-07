a7ed72733e79069d3257f74be67e1289c15d1417
romina.fredes <romina.fredes@estudiantes.utec.edu.uy>
2023-11-07 10:01:03 -0300

    Creando el proyecto.

---
+++ Laboratorio III_Emilio, César, Romina/Laboratorio III_Emilio, César, Romina/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 11/7/2023 10:00:19 AM
+ *  Author: romin
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
