9af98b1b97e29310a212d580d34b467aa7822996
romina.fredes <romina.fredes@estudiantes.utec.edu.uy>
2023-11-07 11:04:47 -0300

    Proyecto nuevo
    El otro se bugueó

---
+++ Laboratorio III/Laboratorio III/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 11/7/2023 11:03:38 AM
+ *  Author: romin
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
